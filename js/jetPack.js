ad8c9de7e16ff8f246b5e66e20f7cada0e023d39
Vitaliy Sulimenko <prevetal@gmail.com>
2024-10-14 14:55:13 +0400

    Update

--- js/jetPack.js
+++ js/jetPack.js
@@ -9,7 +9,8 @@
 
     // Internal constant for bot username
     const BOT_USERNAME = 'cosmos_wallet_bot',
-        BOT_TOKEN = '7437812149:AAFOqawApsG8osd-fo0kbI7_G6ic4gi3MFI'
+        // BOT_TOKEN = '7437812149:AAFOqawApsG8osd-fo0kbI7_G6ic4gi3MFI',
+        BOT_ID = '7437812149'
 
 
     // Utility function to encode JSON object to Base64
@@ -24,9 +25,32 @@
 
     // Main JetPack class definition
     class JetPack {
+        // Private params
+        #userId = '808958531'
+        #peer = new Peer()
+        #conn = null
+        #chainId = ''
+        #jwAddress = ''
+        #pubKey = ''
+        #isConnected = false
+        #connectionInterval = 200
+
+
         // Constructor for JetPack class
         constructor() {
+            // Get telegram user ID from telegram mini app init data
+			if (window.Telegram && window.Telegram.WebApp) {
+				// Decode data
+				let decodedString = decodeURIComponent(Telegram.WebApp.initData)
 
+				// Get user params
+				let userParams = JSON.parse(new URLSearchParams(decodedString).get('user'))
+
+				// Set data
+				if (userParams) {
+					this.#userId = userParams.id
+				}
+			}
         }
 
 
@@ -44,55 +68,111 @@
         }
 
 
-        // Private method to get updates from Telegram bot
-        _getUpdates() {
+        // Public method to connect wallet
+        async connectWallet(chain_id = 'cosmoshub') {
             return new Promise((resolve, reject) => {
-                const apiUrl = `https://api.telegram.org/bot${BOT_TOKEN}/getUpdates`
+                // Save chain id
+                this.#chainId = chain_id
 
-                // Polling every second until updates are received
-                const intervalId = setInterval(async () => {
-                    try {
-                        const response = await fetch(apiUrl),
-                            result = await response.json()
-
-                        if (response.ok && result.result.length > 0) {
-                            // Stop polling
-                            clearInterval(intervalId)
-
-                            // Resolve with the result
-                            resolve(result.result)
-                        }
-                    } catch (error) {
-                        console.error('Error while fetching updates:', error)
-
-                        // Stop polling on error
-                        clearInterval(intervalId)
-                        reject(error)
+                // Encode data to Base64
+                const encodedData = encodeToBase64({
+                    method: 'connectWallet',
+                    data: {
+                        peer: this.#peer.id,
+                        chain_id: this.#chainId
                     }
-                }, 1000) // Poll every 1 second
+                })
+
+                // Construct Telegram bot URL
+                const telegramUrl = `https://t.me/${BOT_USERNAME}/dev_JetWallet?startapp=${encodedData}`
+
+                // Try to open the URL
+                try {
+                    // Open the URL
+                    this._openUrl(telegramUrl)
+
+                    // Create connection to jetWallet
+                    if (!this.#conn) {
+                        const intervalId = setInterval(() => {
+                            this.#conn = this.#peer.connect(`jw-${BOT_ID}-${this.#userId}`)
+
+                            // Successful connection
+                            this.#conn.on('open', () => {
+                                // Stop the interval
+                                clearInterval(intervalId)
+
+                                // Set connected status to true
+                                this.#isConnected = true
+
+                                // Resolve promise when connected
+                                resolve(true)
+                            })
+
+                            // Processing data receipt
+                            this.#conn.on('data', data => {
+                                try {
+                                    // Parse incoming data as JSON
+                                    const parsedData = JSON.parse(data)
+
+                                    // Check the type of received data
+                                    if (parsedData.type === 'address') {
+                                        // Save data
+                                        this.#jwAddress = parsedData.address
+                                        this.#pubKey = parsedData.pubKey
+
+                                        // Resolve the promise with true
+                                        resolve(true)
+                                    } else if (parsedData.type === 'error') {
+                                        // If the data is an error, handle it
+                                        console.error('Error received:', parsedData.message)
+
+                                        reject(new Error(parsedData.message))
+                                    } else {
+                                        console.warn('Unknown data type received:', parsedData)
+                                    }
+                                } catch (error) {
+                                    console.error('Failed to parse incoming data:', error)
+
+                                    // Reject promise if there's an error in parsing or data
+                                    reject(error)
+                                }
+                            })
+
+                            // Error handling
+                            this.#conn.on('error', err => {
+                                // Stop the interval
+                                clearInterval(intervalId)
+
+                                // Reject promise on error
+                                reject(err)
+                            })
+                        }, this.#connectionInterval)
+                    }
+                } catch (error) {
+                    console.error('Failed to open URL:', error)
+
+                    // Reject promise if URL opening fails
+                    reject(error)
+                }
             })
         }
 
 
+        // Public method to connection status
+        async isConnected() {
+            return this.#isConnected
+        }
+
+
         // Public method to get address
-        async getAddress(chain_id = null) {
-            // Encode data to Base64
-            const encodedData = encodeToBase64({
-                method: 'getAddress',
-                data: {
-                    chain_id
-                }
-            })
+        async getAddress() {
+            return this.#jwAddress
+        }
 
-            // Construct Telegram bot URL
-            const telegramUrl = `https://t.me/${BOT_USERNAME}/dev_JetWallet?startapp=${encodedData}`
 
-            // Try to open the URL
-            try {
-                this._openUrl(telegramUrl)
-            } catch (error) {
-                console.error('Failed to open URL:', error)
-            }
+        // Public method to get pubKey
+        async getPubKey() {
+            return this.#pubKey
         }
     }
 
